8271499f29046688afd40ce5ee279871276eabfb
Felix Chan <felixchan29@gmail.com>
2018-07-22 19:00:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,13 @@
 # Write your code here.
 
 substitures = {
-  
+  "hello" becomes 'hi'
+  "to, two, too" become '2' 
+  "for, four" become '4'
+  'be' becomes 'b'
+  'you' becomes 'u'
+  "at" becomes "@" 
+  "and" becomes "&"
 }
 
 def word_substituter(tweet)
